af9f286abe2d4a58e0e8b16bc86ee4c9dd162840
Fayzullo <fayzullarakhmatullayev@gmail.com>
2024-10-25 16:40:53 +0500

    feat: prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import connectDB from './db/connect.js';
 
@@ -13,14 +14,20 @@ import { app, server } from './socket/socket.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 server.listen(PORT, () => {
   connectDB();
